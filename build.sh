f15b33b8a5d5a5fae20c9898da12c475532dece0
CharlsP54 <carlosponce54104@hotmail.com>
2026-07-12 21:06:12 -0500

    Auto crear roles de usuario en DB

--- build.sh
+++ build.sh
@@ -13,6 +13,9 @@ python manage.py collectstatic --no-input
 # Ejecutar migraciones de base de datos
 python manage.py migrate
 
+# Asegurar que los roles básicos existan en la base de datos
+python create_groups.py || true
+
 # Crear superusuario automáticamente si se proveen las credenciales (Útil para planes Free)
 if [[ -n "$DJANGO_SUPERUSER_USERNAME" && -n "$DJANGO_SUPERUSER_PASSWORD" ]]; then
     echo "Intentando crear superusuario automático..."
